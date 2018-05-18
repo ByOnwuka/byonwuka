02e8b8a984624baa63dce63ece24cbb6302e9033
Kushal Hada <kushaldeveloper@gmail.com>
2018-05-18 12:10:39 -0400

    ng generate material table
    [kus@localhost ~]$ cd ~/src/node/byonwuka;
    [kus@localhost byonwuka]$ node --version
    v10.1.0
    [kus@localhost byonwuka]$ npm --version
    5.6.0
    [kus@localhost byonwuka]$ ng --version
    
         _                      _                 ____ _     ___
        / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
       / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
      / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
     /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                    |___/
    
    Angular CLI: 6.0.1
    Node: 10.1.0
    OS: linux x64
    Angular: 6.0.1
    ... animations, cdk, cli, common, compiler, compiler-cli, core
    ... forms, http, language-service, material, platform-browser
    ... platform-browser-dynamic, router
    
    Package                           Version
    -----------------------------------------------------------
    @angular-devkit/architect         0.6.1
    @angular-devkit/build-angular     0.6.1
    @angular-devkit/build-optimizer   0.6.1
    @angular-devkit/core              0.6.1
    @angular-devkit/schematics        0.6.1
    @ngtools/webpack                  6.0.1
    @schematics/angular               0.6.1
    @schematics/update                0.6.1
    rxjs                              6.1.0
    typescript                        2.7.2
    webpack                           4.6.0
    
    [kus@localhost byonwuka]$ ng generate @angular/material:material-table --name=price-table
    CREATE src/app/price-table/price-table-datasource.ts (3387 bytes)
    CREATE src/app/price-table/price-table.component.css (0 bytes)
    CREATE src/app/price-table/price-table.component.html (893 bytes)
    CREATE src/app/price-table/price-table.component.spec.ts (640 bytes)
    CREATE src/app/price-table/price-table.component.ts (725 bytes)
    UPDATE src/app/app.module.ts (1018 bytes)
    [kus@localhost byonwuka]$ git status
    On branch master
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   src/app/app.module.ts
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	src/app/price-table/
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { PriceTableComponent } from './price-table/price-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    PriceTableComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +23,10 @@ import { LayoutModule } from '@angular/cdk/layout';
     LayoutModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
