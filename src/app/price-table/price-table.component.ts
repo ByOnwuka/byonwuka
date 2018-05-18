5d8aa30153e19ebce55c04d287bcf837c27fe5aa
Kushal Hada <kushaldeveloper@gmail.com>
2018-05-18 12:46:08 -0400

    add data tables
    $ git status
    On branch master
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   src/app/app.component.css
    	modified:   src/app/app.component.html
    	modified:   src/app/price-table/price-table-datasource.ts
    	modified:   src/app/price-table/price-table.component.css
    	modified:   src/app/price-table/price-table.component.html
    	modified:   src/app/price-table/price-table.component.ts
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/app/price-table/price-table.component.ts
+++ src/app/price-table/price-table.component.ts
@@ -13,7 +13,7 @@ export class PriceTableComponent implements OnInit {
   dataSource: PriceTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'name', 'amount'];
 
   ngOnInit() {
     this.dataSource = new PriceTableDataSource(this.paginator, this.sort);
